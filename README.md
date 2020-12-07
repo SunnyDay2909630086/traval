e0b3385168853f4ae7ed4f5a087651c54824c200
SunnyDay2909630086 <2909630086@qq.com>
2020-12-08 01:46:09 +0800

    modify some files

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 > a vue-project about travel
  border.css 解决1px边框在多倍分辨显示屏下显示不准确的问题，
- fastclick 解决移动端显示屏下点击延迟300ms的问题
+ fastclick 解决移动端显示屏下点击延迟300ms的问题,
+ 待解决问题：
+ 1.轮播图不能自动切换
 
 ## Build Setup
 
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="wrapper" ref="caroucel">
-    <swiper v-if="swiperList.length > 1" ref="mySwiper" :options="swiperOptions" >
+    <swiper
+      v-if="swiperList.length > 1"
+      ref="mySwiper"
+      :options="swiperOptions"
+    >
       <swiper-slide v-for="item in swiperList" :key="item.id">
-          <img class="swiper-img" :src="item.imgUrl" alt=""/>
+        <img class="swiper-img" :src="item.imgUrl" alt="" />
       </swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
@@ -10,23 +14,23 @@
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-import SwiperCore, { Pagination } from 'swiper'
-import 'swiper/swiper-bundle.css'
-import 'swiper/components/pagination/pagination.less'
-SwiperCore.use([Pagination])
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import SwiperCore, { Pagination } from "swiper";
+import "swiper/swiper-bundle.css";
+import "swiper/components/pagination/pagination.less";
+SwiperCore.use([Pagination]);
 export default {
-  name: 'HomeSwiper',
+  name: "HomeSwiper",
   components: {
     Swiper,
     SwiperSlide
   },
-  data () {
+  data() {
     return {
       swiperOptions: {
         pagination: {
-            el: '.swiper-pagination',
-            bulletActiveClass: 'my-bullet-active'
+          el: ".swiper-pagination",
+          bulletActiveClass: "my-bullet-active"
         },
         autoplay: true,
         loop: true,
@@ -34,50 +38,52 @@ export default {
         observeParents: true
       },
       swiperList: [
-          {
-              id: 1,
-              imgUrl: 'https://imgs.qunarzz.com/vs_ceph_vcimg/8f45c9a4b40091390793e1c4a5ac3e40.jpeg'
-          },
-          {
-              id: 2,
-            imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/86ca4dc2777b997607e97eb8471dcafd.jpg_890x330_0eee7728.jpg'
-          },
-          {
-              id: 3,
-              imgUrl: 'https://imgs.qunarzz.com/vs_ceph_vcimg/a59f46c137856a29be67446ab9c0ad5d.jpeg'
-          }
+        {
+          id: 1,
+          imgUrl:
+            "https://imgs.qunarzz.com/vs_ceph_vcimg/8f45c9a4b40091390793e1c4a5ac3e40.jpeg"
+        },
+        {
+          id: 2,
+          imgUrl:
+            "https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/86ca4dc2777b997607e97eb8471dcafd.jpg_890x330_0eee7728.jpg"
+        },
+        {
+          id: 3,
+          imgUrl:
+            "https://imgs.qunarzz.com/vs_ceph_vcimg/a59f46c137856a29be67446ab9c0ad5d.jpeg"
+        }
       ]
-    }
+    };
   },
   computed: {
-      swiper () {
-        return this.$refs.mySwiper.$swiper
-      }
-    },
-  mounted () {
-    //   console.log(this.swiper.autoplay)
-      this.swiper.slideTo(3, 1000, false)
-        if (this.swiperOptions.autoplay.running) {
-            alert('是')
-        } else {
-            console.log('不是')
-        }
+    swiper() {
+      return this.$refs.mySwiper.$swiper;
     }
-}
+  },
+  mounted() {
+    // this.swiper.slideTo(3, 1000, false) // 切换到第一个slide，速度为1秒
+    if (this.swiperOptions.autoplay.running) {
+      console.log("是")
+    } else {
+      console.log("不是")
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
-    .wrapper{
-        width: 100%;
-        .swiper-img{
-            width: 100%;
-            height: 232px;
-        }
-    }
+.wrapper {
+  width: 100%;
+  .swiper-img {
+    width: 100%;
+    height: 232px;
+  }
+}
 </style>
 <style type="text/css">
-    .my-bullet-active{
-        background: #fff;
-        opacity: 1;
-    }
+.my-bullet-active {
+  background: #fff;
+  opacity: 1;
+}
 </style>
