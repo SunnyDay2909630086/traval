47e6943d6616abe482dfe76d57701a86024d3439
SunnyDay2909630086 <2909630086@qq.com>
2021-03-10 16:40:57 +0800

    add search-box in city

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="search">
+            <input type="text" class="search-input" placeholder="输入城市名或拼音">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CitySearch'
+}
+</script>
+
+<style lang="less" scoped>
+@import '~styles/variable.less';
+    .search{
+        padding: 0.25rem;
+        background-color: @bgColor;
+        .search-input{
+            width: 100%;
+            height: 1.5rem;
+            // padding: 0 .25rem;
+            text-align: center;
+            border-radius: 0.15rem;
+        }
+    }
+</style>
